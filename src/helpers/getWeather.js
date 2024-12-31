150e633eeec610e8ee15d22eb2aef0bc19e1c7e3
Jesus Hector Pacheco Rodriguez <prjesushector@hotmail.com>
2024-12-31 13:37:33 -0600

    Se crea propiedad en componente hijo de padre

---
+++ src/helpers/getWeather.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=25.5439&longitude=-103.419&current=temperature_2m&timezone=auto&forecast_days=1';
+
+
+export const getTemperatura = async () => {
+    const respuesta = await axios.get(API_URL);
+
+    const nuevaTemperatura = respuesta.data.current.temperature_2m;
+
+    return nuevaTemperatura;
+};
